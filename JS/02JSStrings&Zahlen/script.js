78e4119a8fce9c7612f2073b7521919a52e8b3e3
ManuelSagmeister <manuel@pop-os.localdomain>
2022-04-19 09:38:52 +0200

    02JSStrings&Zahlen done

---
+++ JS/02JSStrings&Zahlen/script.js
@@ -0,0 +1,16 @@
+//Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461...
+
+let a = 17.4;
+let b = 3.9;
+
+let result = a/b;
+console.log("The result of "+ a + " divided by " + b + " is " +result);
+
+//Lege eine Variable firstname und eine Variable lastname an. Weise den Variablen Deinen Namen zu. Gib Folgendes aus:
+//"Ich heiße VORNAME NACHNAME"
+
+let firstname = "Manuel";
+let lastname = "Sagmeister";
+
+console.log("Ich heiße " +firstname + " " + lastname)
