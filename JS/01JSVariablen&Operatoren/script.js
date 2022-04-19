575ab62ba3097bf76891c19b4685b54fed0ba287
ManuelSagmeister <manuel@pop-os.localdomain>
2022-04-19 09:30:47 +0200

    01JSVariablen&Operatoren done

---
+++ JS/01JSVariablen&Operatoren/script.js
@@ -0,0 +1,49 @@
+console.log("Hallo");
+
+console.log("Tolle Sache");
+
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+//Berechne folgendes und gib das Ergebnis über console.log aus
+//a+b+c
+
+let resultOne = a+b+c;
+console.log(resultOne);
+
+//(a+b)/c
+
+let resultTwo = (a+b)/c;
+console.log(resultTwo);
+
+//c-a+b
+
+let resultThree = c-a+b;
+console.log(resultThree);
+
+//a+b+c*3
+
+let resultFour = a+b+c*3;
+console.log(resultFour);
+
+//(a+b+c)/4
+
+let resultFive = (a+b+c)/4;
+console.log(resultFive);
+
+//Ändere die Variablen auf folgende Werte
+
+//a=3
+a = 3;
+
+//b=a+3
+b = a+3;
+
+//c=c-3
+c = c-3;
+
+
+console.log(a, b, c);
