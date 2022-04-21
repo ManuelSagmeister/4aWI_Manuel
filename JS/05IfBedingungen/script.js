f4359fcffb1a850ac5dca748b2764f272e9355d5
ManuelSagmeister <manuel@pop-os.localdomain>
2022-04-21 08:03:19 +0200

    05IfBedingungen done

---
+++ JS/05IfBedingungen/script.js
@@ -0,0 +1,28 @@
+//erstelle eine Variable number
+let a = 187;
+
+//Aufgbae 1
+//Wenn die Zahl kleiner ist als 20 gibt aus "Mini"
+//Wenn die Zahl zwischen 20 und 50 ist gib aus "Medium"
+//Wenn die Zahl größer als 50 ist gib aus "Large"
+
+if (a<20){
+    console.log("Mini")
+}else if (a>20 && a<50){
+    console.log("Medium")
+}else {
+    console.log("Large")
+}
+
+//Aufgabe 2
+//Wenn die Zahl kleiner ist als 7 gibt aus "Mini"
+//Wenn die Zahl zwischen 7 und 90 ist gib aus "Medium"
+//Wenn die Zahl größer als 90 ist gib aus "Large"
+
+if (a<20){
+    console.log("Mini")
+}else if (a>20 && a<50){
+    console.log("Medium")
+}else {
+    console.log("Large")
+}
