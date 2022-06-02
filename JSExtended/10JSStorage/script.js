84f08cabb977c1b1d10c99e92ca3ce09a2245682
ManuelSagmeister <manuel@pop-os.localdomain>
2022-06-02 09:26:33 +0200

    10JSStorage done

---
+++ JSExtended/10JSStorage/script.js
@@ -0,0 +1,11 @@
+localStorage.setItem("name", "Bob");
+console.log(localStorage.getItem("name"));
+
+sessionStorage.setItem("name", "John");
+console.log(sessionStorage.getItem("name"));
+sessionStorage.removeItem("name");
+
+document.cookie = "name=Kyle; expires=" + new Date(9999,0,1).toUTCString();
+document.cookie = "lastName=Smith; expires=" + new Date(9999,0,1).toUTCString();
+
+console.log(document.cookie)
